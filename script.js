1020a47b0cf22085fc6891555f4e48aa71d4ffa2
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2022-10-11 17:17:36 +0100

    structre of the Vis

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 const margin = { top: 20, right: 30, bottom: 40, left: 90 };
 const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+const height = 800 - margin.top - margin.bottom;
 
-function init() {}
+function init() {
+    createCustomLineChart("#customLineChart")
+    createDualAxisLineChart("#dualLineChart")
+    createSankeyDiagram("#sankey")
+    createHeatmap("#heatMap")
+}
 
-function createCustomLineChart(){}
-function createDualAxisLineChart(){}
-function createHeatmap(){}
-function createSankeyDiagram(){}
+function createCustomLineChart(id){
+    const svg = d3
+    .select(id)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", (height + margin.top + margin.bottom) / 2);
+}
+
+function createDualAxisLineChart(id){
+    const svg = d3
+    .select(id)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", (height + margin.top + margin.bottom) / 2);
+}
+
+function createHeatmap(id){
+    const svg = d3
+    .select(id)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", (height + margin.top + margin.bottom) / 2);
+}
+
+function createSankeyDiagram(id){
+    const svg = d3
+    .select(id)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+}
