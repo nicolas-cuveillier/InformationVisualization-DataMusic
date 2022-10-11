c6a998f56de9967f1096b129c2922d409702adf6
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2022-10-11 16:48:02 +0100

    basic structure of script file

---
+++ script.js
@@ -0,0 +1,10 @@
+const margin = { top: 20, right: 30, bottom: 40, left: 90 };
+const width = 600 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+
+function init() {}
+
+function createCustomLineChart(){}
+function createDualAxisLineChart(){}
+function createHeatmap(){}
+function createSankeyDiagram(){}
