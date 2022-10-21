c288f1d8459140a3e733e33f07e12f04334b60ae
Marie Hovland <marhovla@stud.ntnu.no>
2022-10-21 12:36:49 +0100

    Sankey works with V7

--- createAndUpdateCharts.js
+++ createAndUpdateCharts.js
@@ -3,7 +3,7 @@ const width = 600 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom; */
 
 function init() {
-  /* createSankeyChart("#vi1"); */
+  createSankeyChart("#sankey");
   /* createScatterPlot("#vi2");
   createLineChart("#vi3"); */
   d3.select("#first").on("click", () => {
@@ -28,7 +28,9 @@ function init() {
   });
 }
 
-/* function createSankeyChart(id) { */
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
 var units = "albums";
 var decade = null;
 var source = null;
@@ -70,162 +72,166 @@ var svg = d3
 var sankey = d3.sankey().nodeWidth(36).nodePadding(40).size([width, height]);
 var path = sankey.link();
 
-// load the data
-d3.csv("sankey_chart.csv", function (error, data) {
-  //set up graph in same style as original example but empty
-  graph = { nodes: [], links: [] };
-  data.forEach(function (d) {
-    graph.nodes.push({ name: d.source });
-    graph.nodes.push({ name: d.target });
-    graph.links.push({
-      source: d.source,
-      target: d.target,
-      value: +d.value,
-      row: d.row,
-    });
-  });
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-  // return only the distinct / unique nodes
-  graph.nodes = d3.keys(
-    d3
-      .nest()
-      .key(function (d) {
+function createSankeyChart(id) {
+  // load the data
+  d3.csv("sankey_chart.csv", function (error, data) {
+    //set up graph in same style as original example but empty
+    graph = { nodes: [], links: [] };
+    data.forEach(function (d) {
+      graph.nodes.push({ name: d.source });
+      graph.nodes.push({ name: d.target });
+      graph.links.push({
+        source: d.source,
+        target: d.target,
+        value: +d.value,
+        row: d.row,
+      });
+    });
+
+    // return only the distinct / unique nodes
+    graph.nodes = d3.keys(
+      d3
+        .nest()
+        .key(function (d) {
+          return d.name;
+        })
+        .object(graph.nodes)
+    );
+
+    // loop through each link replacing the text with its index from node
+    graph.links.forEach(function (d, i) {
+      graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+      graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+    });
+
+    // now loop through each nodes to make nodes an array of objects
+    // rather than an array of strings
+    graph.nodes.forEach(function (d, i) {
+      graph.nodes[i] = { name: d };
+    });
+
+    sankey.nodes(graph.nodes).links(graph.links).layout(32);
+
+    // add in the links
+    var link = svg
+      .append("g")
+      .selectAll(".link")
+      .data(graph.links)
+      .enter()
+      .append("path")
+      .attr("class", "link")
+      .attr("d", path)
+      .style("stroke-width", function (d) {
+        return Math.max(1, d.dy);
+      })
+      .style("stroke-opacity", function (d) {
+        return "0.2";
+      })
+      .sort(function (a, b) {
+        return b.dy - a.dy;
+      });
+
+    // add the link titles
+    link.append("title").text(function (d) {
+      return d.source.name + " → " + d.target.name + "\n" + format(d.value);
+    });
+
+    // add in the nodes
+    var node = svg
+      .append("g")
+      .selectAll(".node")
+      .data(graph.nodes)
+      .enter()
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
+      .call(
+        d3
+          .drag()
+          .subject(function (d) {
+            return d;
+          })
+          .on("start", function () {
+            this.parentNode.appendChild(this);
+          })
+          .on("drag", dragmove)
+      );
+
+    d3.selectAll(".node")
+      .on("mouseover", function (d, i) {
+        source = d.name;
+        link.style("stroke-opacity", function (d) {
+          if (source === d.source.name) {
+            rows.push(d.row);
+          }
+          return rows.includes(d.row) ? "0.3" : "0.05";
+        });
+      })
+      .on("mouseleave", function (d, i) {
+        rows = [];
+        source = null;
+        link.style("stroke-opacity", function (d) {
+          return "0.2";
+        });
+      });
+
+    // add the rectangles for the nodes
+    node
+      .append("rect")
+      .attr("height", function (d) {
+        return d.dy > 10 ? d.dy : 10;
+      })
+      .attr("width", sankey.nodeWidth())
+      .style("fill", function (d) {
+        return (d.color = color(d.name.replace(/ .*/, "")));
+      })
+      .style("stroke", function (d) {
+        return d3.rgb(d.color).darker(2);
+      })
+      .append("title")
+      .text(function (d) {
+        return d.name + "\n" + format(d.value);
+      });
+
+    // add in the title for the nodes
+    node
+      .append("text")
+      .attr("x", -6)
+      .attr("y", function (d) {
+        return d.dy / 2;
+      })
+      .attr("dy", ".35em")
+      .attr("text-anchor", "end")
+      .attr("transform", null)
+      .text(function (d) {
         return d.name;
       })
-      .object(graph.nodes)
-  );
+      .filter(function (d) {
+        return d.x < width / 2;
+      })
+      .attr("x", 6 + sankey.nodeWidth())
+      .attr("text-anchor", "start");
 
-  // loop through each link replacing the text with its index from node
-  graph.links.forEach(function (d, i) {
-    graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
-    graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+    // the function for moving the nodes
+    function dragmove(d) {
+      d3.select(this).attr(
+        "transform",
+        "translate(" +
+          d.x +
+          "," +
+          (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) +
+          ")"
+      );
+      sankey.relayout();
+      link.attr("d", path);
+    }
   });
-
-  // now loop through each nodes to make nodes an array of objects
-  // rather than an array of strings
-  graph.nodes.forEach(function (d, i) {
-    graph.nodes[i] = { name: d };
-  });
-
-  sankey.nodes(graph.nodes).links(graph.links).layout(32);
-
-  // add in the links
-  var link = svg
-    .append("g")
-    .selectAll(".link")
-    .data(graph.links)
-    .enter()
-    .append("path")
-    .attr("class", "link")
-    .attr("d", path)
-    .style("stroke-width", function (d) {
-      return Math.max(1, d.dy);
-    })
-    .style("stroke-opacity", function (d) {
-      return "0.2";
-    })
-    .sort(function (a, b) {
-      return b.dy - a.dy;
-    });
-
-  // add the link titles
-  link.append("title").text(function (d) {
-    return d.source.name + " → " + d.target.name + "\n" + format(d.value);
-  });
-
-  // add in the nodes
-  var node = svg
-    .append("g")
-    .selectAll(".node")
-    .data(graph.nodes)
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", function (d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    })
-    .call(
-      d3
-        .drag()
-        .subject(function (d) {
-          return d;
-        })
-        .on("start", function () {
-          this.parentNode.appendChild(this);
-        })
-        .on("drag", dragmove)
-    );
-
-  d3.selectAll(".node")
-    .on("mouseover", function (d, i) {
-      source = d.name;
-      link.style("stroke-opacity", function (d) {
-        if (source === d.source.name) {
-          rows.push(d.row);
-        }
-        return rows.includes(d.row) ? "0.3" : "0.05";
-      });
-    })
-    .on("mouseleave", function (d, i) {
-      rows = [];
-      source = null;
-      link.style("stroke-opacity", function (d) {
-        return "0.2";
-      });
-    });
-
-  // add the rectangles for the nodes
-  node
-    .append("rect")
-    .attr("height", function (d) {
-      return d.dy > 10 ? d.dy : 10;
-    })
-    .attr("width", sankey.nodeWidth())
-    .style("fill", function (d) {
-      return (d.color = color(d.name.replace(/ .*/, "")));
-    })
-    .style("stroke", function (d) {
-      return d3.rgb(d.color).darker(2);
-    })
-    .append("title")
-    .text(function (d) {
-      return d.name + "\n" + format(d.value);
-    });
-
-  // add in the title for the nodes
-  node
-    .append("text")
-    .attr("x", -6)
-    .attr("y", function (d) {
-      return d.dy / 2;
-    })
-    .attr("dy", ".35em")
-    .attr("text-anchor", "end")
-    .attr("transform", null)
-    .text(function (d) {
-      return d.name;
-    })
-    .filter(function (d) {
-      return d.x < width / 2;
-    })
-    .attr("x", 6 + sankey.nodeWidth())
-    .attr("text-anchor", "start");
-
-  // the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this).attr(
-      "transform",
-      "translate(" +
-        d.x +
-        "," +
-        (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) +
-        ")"
-    );
-    sankey.relayout();
-    link.attr("d", path);
-  }
-});
-/* } */
+}
 
 function createScatterPlot(id) {
   const svg = d3
