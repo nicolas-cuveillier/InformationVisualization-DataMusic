c32770f8f510f6ce999b2f560ef78b2e78b68798
Marie Hovland <marhovla@stud.ntnu.no>
2022-10-22 13:07:13 +0100

    Sankey layout adjustments

--- sankey.js
+++ sankey.js
@@ -60,8 +60,8 @@ d3.sankey = function () {
         xi = d3.interpolateNumber(x0, x1),
         x2 = xi(curvature),
         x3 = xi(1 - curvature),
-        y0 = (d.source.y + d.sy + d.dy / 2) * 0.9,
-        y1 = (d.target.y + d.ty + d.dy / 2) * 0.9;
+        y0 = (d.source.y + d.sy + d.dy / 2) * 0.85,
+        y1 = (d.target.y + d.ty + d.dy / 2) * 0.85;
       return (
         "M" +
         x0 +
