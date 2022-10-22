922e1a472c186f98f29e30cdf74982938dd04e7b
Marie Hovland <marhovla@stud.ntnu.no>
2022-10-22 14:18:56 +0100

    Rebase

--- createCSV.py
+++ createCSV.py
@@ -3,11 +3,14 @@
 import pandas as pd
 
 data = pd.read_csv('final.csv')
+decade = "_10s"
+start = 200
+end = 300
 
-df = data.iloc[0:len(data)]
+df = data.iloc[start:end]
 df = df[['Genre', 'Album Length', 'Worldwide Sales (Est.)']]
 
-for i in range(len(data)):
+for i in range(start, end):
     album_length = float(df.loc[i]['Album Length'])
     worldwide_sales = float((df.loc[i]['Worldwide Sales (Est.)']).replace(',', ''))
 
@@ -21,21 +24,21 @@ for i in range(len(data)):
         df.loc[i, 'Album Length'] = '>80'
 
     if worldwide_sales >= 20000000:
-        df.loc[i, 'Worldwide Sales (Est.)'] = '>20,000,000'
+        df.loc[i, 'Worldwide Sales (Est.)'] = '>20'
     elif worldwide_sales >= 15000000:
-        df.loc[i, 'Worldwide Sales (Est.)'] = '15,000,000-20,000,000'
+        df.loc[i, 'Worldwide Sales (Est.)'] = '15-20'
     elif worldwide_sales >= 10000000:
-        df.loc[i, 'Worldwide Sales (Est.)'] = '10,000,000-15,000,000'
+        df.loc[i, 'Worldwide Sales (Est.)'] = '10-15'
     elif worldwide_sales >= 5000000:
-        df.loc[i, 'Worldwide Sales (Est.)'] = '5,000,000-10,000,000'
+        df.loc[i, 'Worldwide Sales (Est.)'] = '5-10'
     else:
-        df.loc[i, 'Worldwide Sales (Est.)'] = '<5,000,000'
+        df.loc[i, 'Worldwide Sales (Est.)'] = '<5'
     
     df.loc[i, 'row'] = str(i)
 
-df.to_csv(f'sankey_data.csv', index=False)
+df.to_csv(f'sankey_data' + decade + '.csv', index=False)
 
-sankey_data = pd.read_csv('sankey_data.csv')
+sankey_data = pd.read_csv('sankey_data' + decade + '.csv')
 print(sankey_data)
 
 df_1 = sankey_data[['Genre', 'Album Length', 'row']]
@@ -49,9 +52,9 @@ df_2.rename(columns={'Album Length': 'source'}, inplace=True)
 df_2.rename(columns={'Worldwide Sales (Est.)': 'target'}, inplace=True)
 df_2['value'] = 1
 
-pd.concat([df_1, df_2]).to_csv(f'sankey_chart.csv', index=False)
+pd.concat([df_1, df_2]).to_csv(f'sankey_chart' + decade + '.csv', index=False)
 
-sankey = pd.read_csv('sankey_chart.csv')
+sankey = pd.read_csv('sankey_chart' + decade + '.csv')
 
 sorted = sankey.pivot_table(columns=['source', 'target'], aggfunc={'value': 'size'}).transpose()
-sorted.to_csv(f'sankey_sorted.csv', index=True)
+sorted.to_csv(f'sankey_sorted' + decade + '.csv', index=True)
